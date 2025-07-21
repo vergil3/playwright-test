65376179a057edb14ce43107c136a94cb3069aa1
l <36623206+vergil3@users.noreply.github.com>
2025-07-21 21:05:13 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
+    //test comment
   await page.goto('https://playwright.dev');
   await expect(page).toHaveTitle(/Playwright/);
 });
